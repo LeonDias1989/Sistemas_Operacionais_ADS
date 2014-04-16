b5792e89ac9b6068e441a9a7f9adc68c127f3b07
Leon <leondias1989@gmail.com>
2014-04-16 10:35:17 -0300

    Inicial
    Criação de classes utilitárias

---
+++ OrdemProcedencia/src/OrdemProcedencia/util/RandomUtil.java
@@ -0,0 +1,16 @@
+package OrdemProcedencia.util;
+
+import java.util.Random;
+
+public class RandomUtil {
+
+	Random r = new Random();
+	
+	public int geradorInt(){
+		
+		return r.nextInt();
+		//TODO DEFINIR INTERVALO
+		
+	}
+
+}
