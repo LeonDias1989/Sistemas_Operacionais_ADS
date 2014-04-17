30528a0d9a88baf2d8fba7ba128e832e2ed0d8e5
jandrei <jandrei.cm@gmail.com>
2014-04-16 23:33:09 -0300

    teste inicial

--- TrabalhoEscalonador/src/fifo/Fifo.java
+++ TrabalhoEscalonador/src/fifo/Fifo.java
@@ -1,7 +1,32 @@
 package fifo;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Fifo {
 	public static void main(String[] args) {
-
+		Queue<Integer> lista = new LinkedList<Integer>();
+		lista.add(1);
+		lista.add(2);
+		lista.add(3);
+		lista.add(4);
+		lista.add(5);
+		
+		Integer valor = lista.peek();
+		System.out.println(valor);
+		lista.remove(valor);
+		
+		valor = lista.peek();
+		System.out.println(valor);
+		lista.remove(valor);
+		
+		valor = lista.peek();
+		System.out.println(valor);
+		lista.remove(valor);
+		
+		valor = lista.peek();
+		System.out.println(valor);
+		lista.remove(valor);
+		
 	}
 }
