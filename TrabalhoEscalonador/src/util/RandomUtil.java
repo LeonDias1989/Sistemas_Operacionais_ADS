8754d6a2a2e6f1b15fdd7823659dd478d40296ba
Leon <leondias1989@gmail.com>
2014-04-23 09:30:20 -0300

    Definido o intervalo de prioridade randômica (1-5)

--- TrabalhoEscalonador/src/util/RandomUtil.java
+++ TrabalhoEscalonador/src/util/RandomUtil.java
@@ -5,12 +5,11 @@ import java.util.Random;
 public class RandomUtil {
 
 	Random r = new Random();
-	
-	public int geradorInt(){
-		
-		return r.nextInt();
-		//TODO DEFINIR INTERVALO
-		
+
+	public int geradorInt() {
+
+		return r.nextInt(5) + 1;
+
 	}
 
 }
