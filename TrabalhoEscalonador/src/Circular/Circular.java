142e44b9224ed7e69f45793da83e2e2a2b00a7cf
jandrei <jandrei.cm@gmail.com>
2014-04-29 00:03:03 -0300

    comentários nas classes

--- TrabalhoEscalonador/src/Circular/Circular.java
+++ TrabalhoEscalonador/src/Circular/Circular.java
@@ -10,8 +10,6 @@ import util.ProcessoTipoOrdenacao;
 import util.Scan;
 
 public class Circular {
-	int maiorPrioridade = -1;
-	int tempoCorrente = 0;
 	int quantum = 2;
 
 	private List<Processo> filaProcessos = new ArrayList<>();
@@ -21,26 +19,13 @@ public class Circular {
 	}
 
 	public Processo adicionar(Processo novoProcesso) {
-
 		filaProcessos.add(novoProcesso);
 		return novoProcesso;
-
 	}
 
 	public Processo remover(Processo processoARemover) {
-
 		filaProcessos.remove(processoARemover);
 		return processoARemover;
-	}
-
-	public List<Processo> ordenarPorChegada() {
-		Collections.sort(filaProcessos, new ProcessoComparator(ProcessoTipoOrdenacao.TEMPO_CHEGADA));
-		return filaProcessos;
-	}
-
-	public List<Processo> ordenarPorDuracao() {
-		Collections.sort(filaProcessos, new ProcessoComparator(ProcessoTipoOrdenacao.TEMPO_DURACAO));
-		return filaProcessos;
 	}
 
 	public boolean todosProcessosFinalizados() {
@@ -55,6 +40,7 @@ public class Circular {
 	}
 
 	public Processo getProximoProcessoAExecutar() {
+		//testo se na lista existentes nao estao todos finalizados
 		if (todosProcessosFinalizados()) {
 			return null;
 		}
@@ -62,16 +48,21 @@ public class Circular {
 		// ordena pela maior prioridade
 		Collections.sort(filaProcessos, new ProcessoComparator(ProcessoTipoOrdenacao.PRIORIDADE, false));
 
+		//como ordenei de forma decrescente conforme o parametro false da classe ProcessoComparator diz
+		//só preciso pegar o primeiro processo da lista que é o de maior prioridade.
 		Processo processo = filaProcessos.get(0);
 		ArrayList<Processo> prioridadesIguais = new ArrayList<Processo>();
 		prioridadesIguais.add(processo);
 
+		//verifico se existe mais algum de prioridade igual ao do encontrado
 		for (Processo proc : filaProcessos) {
 			if (processo.getPrioridade() == proc.getPrioridade()) {
+				//guardo todos de mesma prioridade em uma lista
 				prioridadesIguais.add(proc);
 			}
 		}
 
+		//ordeno os de mesma prioridade pelo tempo de chegada e pego o primeiro
 		if (!prioridadesIguais.isEmpty()) {
 			// ordena pelo menor tempo de chegada
 			Collections.sort(prioridadesIguais, new ProcessoComparator(ProcessoTipoOrdenacao.TEMPO_CHEGADA));
@@ -83,55 +74,71 @@ public class Circular {
 
 	public void executaProcessos() throws Exception {
 		System.out.println("----------------------------");
+		//pego o proximo processo a ser executado.
 		Processo processo = getProximoProcessoAExecutar();
 
+		//garantia de que quando todos processos forem concluidos o algoritmo ira finalizar aqui.
 		if (processo == null) {
 			System.out.println("\n\nfim");
 			return;
 		}
 
+		
 		System.out.println("Executando processo " + processo.getName() + "...");
-		Thread.sleep(1000 * 1);
-		System.out.println("..");
-		Thread.sleep(1000 * 1);
+		for (int i = 0; i < quantum; i++) {
+			//esperando o quantum acabar
+			Thread.sleep(1 * 1000);
+		}
 
+		//diminuo o tempo de duração pois ja executei um pouco do processo
 		processo.setTempoDuracao(processo.getTempoDuracao() - quantum);
+		//diminuo em 1 a prioridade	
 		processo.setPrioridade(processo.getPrioridade() - 1);
 
-		System.out.println("restando " + processo.getTempoDuracao()+" -> "+processo);
+		System.out.println("restando " + processo.getTempoDuracao() + " -> " + processo);
 
+		//se tempo de duração acabou sinal que nao tme mais prioridade e que o procesos finalizou
 		if (processo.getTempoDuracao() <= 0) {
 			processo.setAcabou(true);
 			processo.setPrioridade(0);
 		}
 
+		//manda executar novamente
 		executaProcessos();
 	}
 
 	public static void main(String[] args) throws Exception {
-
+		//Aqui tudo inicia
 		System.out.println("Quantos Processo Deseja Executar?");
 		System.out.println("-------------");
+		//instancia a scan
 		Scan teclado = new Scan();
+		//instancia a classe circular
 		Circular circular = new Circular();
 
-		// Chose number of process that execute
+		// solicita a quantidade de processos que ele vai executar
 		int numeroProcesso = teclado.scanInt();
 
-		// Chose Process
+		// preenche a lista com os processo que ele deseja
 		for (int i = 0; i < numeroProcesso; i++) {
 			System.out.println("-------------");
 
+			//fixo um nome aqui ppara facilitar os testes
 			String nome = "Processo-" + i;
+			//imprimo o nome do processo
 			System.out.println("Criando " + nome);
+			//solicito o tempo de execução desse processo
 			int tempoExecucao = teclado.scanInt("Tempo de Execucao: ");
+			//solicito a prioridade
 			int prioridadeDoProcesso = teclado.scanInt("Prioridade: ");
 
+			//adiciono na lista de processos
 			circular.adicionar(new Processo(nome, circular.getFilaProcessos().size() + 1, tempoExecucao, prioridadeDoProcesso));
 
 			System.out.println(nome + " criado com sucesso !\n\n");
 		}
 
+		//logs para ficar bonito o algoritmo.
 		System.out.println("\n\nColocando os processo na fila de execução.");
 		Thread.sleep(500);
 		System.out.print(".");
@@ -142,6 +149,7 @@ public class Circular {
 		Thread.sleep(500);
 		System.out.print("..\n\n\n\n\n\n\n\n");
 
+		//executo os processos
 		circular.executaProcessos();
 	}
 
