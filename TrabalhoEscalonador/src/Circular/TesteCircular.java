ad3719b94c91b7fafc89ec66071b28e92e04d180
jandrei <jandrei.cm@gmail.com>
2014-04-29 18:44:32 -0300

    ajuste

--- TrabalhoEscalonador/src/Circular/TesteCircular.java
+++ TrabalhoEscalonador/src/Circular/TesteCircular.java
@@ -10,9 +10,9 @@ public static void main(String[] args) throws Exception{
 		Circular filaProcessos = new Circular();
 		
 		//nome,chegada,duracao,prioridade
-		filaProcessos.adicionar(new Processo("A", 1, 10, 2));
-		filaProcessos.adicionar(new Processo("B", 2, 6, 4));
-		filaProcessos.adicionar(new Processo("C", 3, 8, 5));
+		filaProcessos.adicionar(new Processo("P1", 1, 10, 2));
+		filaProcessos.adicionar(new Processo("P2", 2, 6, 4));
+		filaProcessos.adicionar(new Processo("P3", 3, 8, 5));
 		
 		filaProcessos.executaProcessos();
 		
